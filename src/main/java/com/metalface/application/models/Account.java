d0acb7e1190543070f301bb6f52c73ffe1e85d3c
metal-face <hughesbryan3000@gmail.com>
2023-09-09 02:45:02 -0400

    Improvements.
    Lombok setter, getter and table
    decorators were added to the class
    definition.
    
    More explicit decorators such as Basic
    and Temporal were added to respective
    columns.

--- src/main/java/com/metalface/application/models/Account.java
+++ src/main/java/com/metalface/application/models/Account.java
@@ -1,27 +1,48 @@
 package com.metalface.application.models;
 
-
 import com.metalface.application.enums.Role;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+
+import java.util.Date;
 import java.util.UUID;
 
-@Entity(name="accounts")
+@Setter
+@Getter
+@Entity
+@Table(name="accounts")
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="account_id")
     private UUID accountId;
 
+    @Basic
     @Column(name="username", length = 80, nullable = false, unique = true)
     private String username;
 
+    @Basic
     @Column(name = "email", length = 255, nullable = false, unique = true)
     private String email;
 
+    @Basic
     @Column(name="password", length = 255, nullable = false, unique = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
+    @Column(name="role", nullable = false)
     private Role role;
+
+    @Basic
+    @Column(name="created_at")
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
+
+    @Basic
+    @Column(name="updated_at")
+    @Temporal(TemporalType.DATE)
+    private Date updatedAt;
 
 }
