5df9cb38dea9ebed6b71eaf86e2ab8d828a21645
metal-face <hughesbryan3000@gmail.com>
2023-09-08 22:22:20 -0400

    accounts controller added

---
+++ src/main/java/com/metalface/application/api/AccountsController.java
@@ -0,0 +1,34 @@
+
+package com.metalface.application.api;
+
+import com.metalface.application.dto.AccountDTO;
+import com.metalface.application.managers.AccountsManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("accounts")
+public class AccountsController {
+    @Autowired
+    private AccountsManager accountsManager;
+
+    @PostMapping(value = "/", produces = "application/json")
+    public AccountDTO createAccount(@RequestBody AccountDTO account) {
+        return accountsManager.createAccount(account);
+    }
+
+    @GetMapping(value = "/${id}", produces = "application/json")
+    public AccountDTO fetchAccount(@PathVariable UUID id) {
+        return accountsManager.fetchAccount(id);
+    }
+
+    @GetMapping(value = "/", produces = "application/json")
+    public Page<AccountDTO> fetchManyAccounts(@RequestParam(name = "page", defaultValue = "0", required = false) Integer page, @RequestParam(name = "size", defaultValue = "10", required = false) Integer size)
+    {
+        return this.accountsManager.fetchManyAccounts(PageRequest.of(page, size));
+    }
+}
